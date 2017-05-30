0ae338bdb4785d006a5bfeb786c0983443579eb8
sdb31 <samuel.butensky@gmail.com>
2017-05-30 11:26:31 -0400

    Annotated

--- Functions/Core/VermicelliRead.m
+++ Functions/Core/VermicelliRead.m
@@ -1,62 +1,27 @@
-function [data] = VermicelliRead(files);
-% datapath = 'C:\';
-% datapath = uigetdir(datapath, 'Where is your Vermicelli data located?');       %Ask the user where their data is located.
-% if datapath(1) == 0                                                         %If the user pressed "cancel"...
-%     return                                                                  %Skip execution of the rest of the function.
-% end
-% 
-% files = file_miner(datapath,{'*.xls','*.xlsx'});  
-% pause(0.01);                                                                %Pause for 10 milliseconds.
-% if isempty(files)                                                           %If no files were found...
-%     errordlg('No Vermicelli files were found!');   %Show an error dialog box.
-% end
-% % 
-% for i = 1:length(files)
-%     [status,sheets] = xlsfinfo(char(files(i)));
-%     No_Sheets = numel(sheets);
-%     if No_Sheets > 1;
-%         for s = 1:No_Sheets;
-%            T = readtable(char(files(i)),'Sheet', s);         
-%            data(i).animal(s).name = T{1,2};
-%            for q = 1:(size(T,2)/3);
-%                data(i).animal(s).video{q} = T{4:end,3*q-2}';  
-%                data(i).animal(s).targeted{q} = T{4:end,3*q-1}';  
-%                data(i).animal(s).nontargeted{q} = T{4:end,3*q}';  
-%            end
-%            clear T
-%         end
-%     else
-%         T = readtable(char(files(i)));
-%         data(i).animal(1).name = T{1,2};
-%         clear T
-%     end
-%     data(i).filename = files(i);
-%     clear status sheets No_Sheets
-% end
-
-for i = 1:length(files)
-    [status,sheets] = xlsfinfo(char(files(i)));
-    No_Sheets = numel(sheets);
-    if No_Sheets > 1;
-        for s = 1:No_Sheets;
-            [NUM,TXT,RAW] = xlsread(char(files(i)),s);
-            [status,sheets,xlFormat] = xlsfinfo(char(files(i)));
-            data(i).animal(s).name = TXT(2,2);
-            for q = 1:(size(NUM,2)/3);
-                data(i).animal(s).date{q} = TXT{3,3*q-1};
-                data(i).animal(s).video{q} = NUM(1:end,3*q-2)';
-                data(i).animal(s).targeted{q} = NUM(1:end,3*q-1)';
-                data(i).animal(s).nontargeted{q} = NUM(1:end,3*q)';
+function [data] = VermicelliRead(files)
+for i = 1:length(files)                                                                                                 % Step through each Vermicelli Master File
+    [~,sheets] = xlsfinfo(char(files(i)));                                                                         % Read in the info for each sheet within each Master FIle
+    No_Sheets = numel(sheets);                                                                                          % Calculate the number of sheets
+    if No_Sheets > 1;                                                                                                   % If the number of sheets is greater than 1 AKA the number of animals is greater than 1
+        for s = 1:No_Sheets;                                                                                            % Step through each sheet
+            [NUM,TXT,~] = xlsread(char(files(i)),s);                                                                  % Read in number, text, and all raw data from each sheet
+            [~,~,~] = xlsfinfo(char(files(i)));
+            data(i).animal(s).name = TXT(2,2);                                                                          % Read in the animal name from the second row, second column of the TXT matrix
+            for q = 1:(size(NUM,2)/3);                                                                                  % Step through each block of columns per date
+                data(i).animal(s).date{q} = TXT{3,3*q-1};                                                               % Read in the date                
+                data(i).animal(s).video{q} = NUM(1:end,3*q-2)';                                                         % Read in the number of videos
+                data(i).animal(s).targeted{q} = NUM(1:end,3*q-1)';                                                      % Read in the number of targeted manipulations
+                data(i).animal(s).nontargeted{q} = NUM(1:end,3*q)';                                                     % Read in the number of non-targeted manipulations
             end
-            for l = 1:(size(data(i).animal(s).targeted,2));
-                data(i).animal(s).mean_targeted(l) = nanmean(data(i).animal(s).targeted{l});
-                data(i).animal(s).mean_nontargeted(l) = nanmean(data(i).animal(s).nontargeted{l});                
+            for l = 1:(size(data(i).animal(s).targeted,2));                                                             % Calculate average stats for each day
+                data(i).animal(s).mean_targeted(l) = nanmean(data(i).animal(s).targeted{l});                            % Calculate mean number of targeted manipulations
+                data(i).animal(s).mean_nontargeted(l) = nanmean(data(i).animal(s).nontargeted{l});                      % Calculate mean number of non-targeted manipulations
             end
-            data(i).animal(s).ratio = log2(data(i).animal(s).mean_targeted./data(i).animal(s).mean_nontargeted);
-            clear NUM TXT RAW
+            data(i).animal(s).ratio = log2(data(i).animal(s).mean_targeted./data(i).animal(s).mean_nontargeted);        % Calculate the ratio of targeted to non-targeted manipulations
+            clear NUM TXT RAW                                                                                           % Clear the variables before stepping through the next sheet
         end
-    else
-        [NUM,TXT,RAW] = xlsread(char(files(i)));
+    else                                                                                                                % If the number of sheets isn't greater than 1 AKA the number of animals equals 1...
+        [NUM,TXT,~] = xlsread(char(files(i)));                                                                        % ...repeat the steps seen above for 1 animal
         data(i).animal.name = TXT(2,2);
         for q = 1:(size(NUM,2)/3);
             data(i).animal.date{q} = TXT{3,3*q-1};
@@ -71,7 +36,7 @@ for i = 1:length(files)
         data(i).animal.ratio = log2(data(i).animal(s).mean_targeted./data(i).animal(s).mean_nontargeted);
         clear NUM TXT RAW
     end
-    data(i).filename = files(i);
-    data(i).device = 'Vermicelli';
+    data(i).filename = files(i);                                                                                        % Save the Filename
+    data(i).device = 'Vermicelli';                                                                                      % Save the device name. Dexterity requires this for analysis
     clear status sheets No_Sheets
 end
